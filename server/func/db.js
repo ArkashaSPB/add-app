05dbd6e099f155a24d57da61e4a0df63e2842327
ArkashaSPB <arka-24@yandex.ru>
2025-03-23 16:10:23 +0300

    Ваш комментарий к коммиту

--- server/func/db.js
+++ server/func/db.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Получаем значения переменных из .env
-const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
+const { MYSQL_HOST,MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
 
 // Проверяем наличие переменных окружения
 if (!MYSQL_HOST || !MYSQL_USER || !MYSQL_PASSWORD || !MYSQL_DATABASE) {
@@ -18,6 +18,7 @@ export async function connectToDatabase() {
 	try {
 		const connection = await mysql.createConnection({
 			host: MYSQL_HOST,
+			port: Number(MYSQL_PORT) || 3306,
 			user: MYSQL_USER,
 			password: MYSQL_PASSWORD,
 			database: MYSQL_DATABASE,
