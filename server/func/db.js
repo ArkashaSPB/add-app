2c07f8ca64912681b3252b1768eb7b521e7a1f7e
ArkashaSPB <arka-24@yandex.ru>
2025-03-23 16:13:50 +0300

    Ваш комментарий к коммиту

--- server/func/db.js
+++ server/func/db.js
@@ -5,25 +5,23 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Получаем значения переменных из .env
-const { MYSQL_HOST,MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
+const { MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
 
-// Проверяем наличие переменных окружения
 if (!MYSQL_HOST || !MYSQL_USER || !MYSQL_PASSWORD || !MYSQL_DATABASE) {
 	console.error('❌ Ошибка: отсутствуют необходимые переменные окружения в .env!');
 	process.exit(1);
 }
 
-// Функция для подключения к MySQL
 export async function connectToDatabase() {
 	try {
 		const connection = await mysql.createConnection({
 			host: MYSQL_HOST,
-			port: Number(MYSQL_PORT) || 3306,
+			port: Number(MYSQL_PORT) || 3306, // По умолчанию 3306
 			user: MYSQL_USER,
 			password: MYSQL_PASSWORD,
 			database: MYSQL_DATABASE,
-			namedPlaceholders: true, // Позволяет использовать `:param` в запросах
-			connectTimeout: 10000, // Таймаут подключения (10 сек.)
+			namedPlaceholders: true,
+			connectTimeout: 10000,
 		});
 
 		// console.log('✅ Подключение к базе данных успешно!');
